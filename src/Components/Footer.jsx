08fd3dc9ad66eb39647909847a47da6b4984fde0
khan-fardin <1120f.khan@gmail.com>
2025-08-09 12:00:39 +0600

    footer edit1

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    {/* <a className="link link-hover">Press kit</a> */}
                 </nav>
                 <nav>
                     <h6 className="footer-title">Legal</h6>
